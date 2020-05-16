fcecd92f7c9b4ea3b8250094d749e3f0638db1cf
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 15:40:35 +0200

    Adjusted User entity

--- src/main/java/tim11osa/email/main_app/model/User.java
+++ src/main/java/tim11osa/email/main_app/model/User.java
@@ -1,4 +1,107 @@
 package tim11osa.email.main_app.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true, nullable = false)
+    private int id;
+
+    @Column(name = "first_name", unique = false, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", unique = false, nullable = false)
+    private String lastName;
+
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", unique = true, nullable = false)
+    private String password;
+
+    @Column(name = "roles", unique = false, nullable = false)
+    private String roles;
+
+    public User(){
+
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password, String roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+/*    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }*/
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
