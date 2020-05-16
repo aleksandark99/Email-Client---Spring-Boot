d448bef18e3ea78759ce9d925060eaae0d9c00eb
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 15:46:36 +0200

    Added Contact entity

---
+++ src/main/java/tim11osa/email/main_app/model/Contact.java
@@ -0,0 +1,116 @@
+package tim11osa.email.main_app.model;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "contact_id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "firstName", unique = false, nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", unique = false, nullable = false)
+    private String lastName;
+
+    @Column(name = "displayName", unique = false, nullable = false)
+    private String displayName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "photoPath", unique = false, nullable = false)
+    private String photoPath;
+
+    @Column(name = "note", unique = false, nullable = false)
+    private String note;
+
+
+    public Contact(String firstName, String lastName, String displayName, String email, String photoPath, String note) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.displayName = displayName;
+        this.email = email;
+        this.photoPath = photoPath;
+        this.note = note;
+    }
+
+    public Contact(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", email='" + email + '\'' +
+                ", photoPath='" + photoPath + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
