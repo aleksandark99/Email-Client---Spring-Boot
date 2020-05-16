aa702644532ef287d894618edd77acebf19f2f21
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 21:06:37 +0200

    Added SecurityAdapter

---
+++ src/main/java/tim11osa/email/main_app/security/SecurityAdapter.java
@@ -0,0 +1,6 @@
+package tim11osa.email.main_app.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+}
