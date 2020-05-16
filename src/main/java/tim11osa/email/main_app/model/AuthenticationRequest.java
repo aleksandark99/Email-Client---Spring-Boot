7315c336ec47b2ce86b01dc93d6e99cbe18b3a71
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 21:14:56 +0200

    Added AuthenticationRequest

---
+++ src/main/java/tim11osa/email/main_app/model/AuthenticationRequest.java
@@ -0,0 +1,32 @@
+package tim11osa.email.main_app.model;
+
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
+    private AuthenticationRequest(){
+
+    }
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
