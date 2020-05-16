b9039bdf60cc39232f11ca0b3474668503d4cb6d
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 21:27:39 +0200

    Added JwtRequestFilter & JwtUtility

--- src/main/java/tim11osa/email/main_app/security/LoggedUserDetailsService.java
+++ src/main/java/tim11osa/email/main_app/security/LoggedUserDetailsService.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import tim11osa.email.main_app.model.User;
 import tim11osa.email.main_app.repository.UserRepository;
 
 import java.util.Optional;
 
+@Service
 public class LoggedUserDetailsService implements UserDetailsService {
 
     @Autowired
