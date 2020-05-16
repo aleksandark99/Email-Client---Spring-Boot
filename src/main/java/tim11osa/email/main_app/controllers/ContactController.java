9590756bee3a77e97f3189f752599430428f3edc
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 20:43:49 +0200

    Added empty contollers for User & Contact

---
+++ src/main/java/tim11osa/email/main_app/controllers/ContactController.java
@@ -0,0 +1,17 @@
+package tim11osa.email.main_app.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/")
+public class ContactController {
+
+    
+}
