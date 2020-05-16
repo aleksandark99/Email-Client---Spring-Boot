5b343c1b2f4d9c98340dc4c5f2c51ee72026eb78
Aleksandar Kotur <aleksa.kotur@gmail.com>
2020-05-16 13:56:20 +0200

    initial project commit

---
+++ src/main/java/tim11osa/email/main_app/MainAppApplication.java
@@ -0,0 +1,13 @@
+package tim11osa.email.main_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tim11osa/email/main_app/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tim11osa.email.main_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
