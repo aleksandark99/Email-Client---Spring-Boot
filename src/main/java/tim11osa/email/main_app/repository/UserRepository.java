10cdc1fd592501c00dcc055fc36afa72a27070d1
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-16 15:41:37 +0200

    Adjusted UserRepository

--- src/main/java/tim11osa/email/main_app/repository/UserRepository.java
+++ src/main/java/tim11osa/email/main_app/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package tim11osa.email.main_app.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tim11osa.email.main_app.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsernameOrPassword(String username, String password);
+    Optional<User> findByUsername(String username);
+
 }
