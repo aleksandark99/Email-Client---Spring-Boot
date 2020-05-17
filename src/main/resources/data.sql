889dfce5125d8558628234e6348390c1c186a7b7
MiljanGitHub <miljan.puletic@gmail.com>
2020-05-17 22:57:37 +0200

    Added data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (user_id, first_name, last_name, user_name, pass_word, roles)
-VALUES (1, 'a', 'a','a','a','a', 'ROLE_USER');
 
-INSERT INTO user (user_id, first_name, last_name, user_name, pass_word, roles)
-VALUES (2, 'b', 'b','b','b','b', 'ROLE_USER');
+
+INSERT INTO `email_project`.`user` ( first_name, last_name, user_name, pass_word, roles) VALUES ( 'a','a','a','a', 'ROLE_USER');
+
+INSERT INTO `email_project`.`user` (first_name, last_name, user_name, pass_word, roles)VALUES ( 'b','b','b','b', 'ROLE_USER');
