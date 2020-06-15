37419d760e21d1d0125af8eea36d96bde3e6f2f0
MiljanGitHub <miljan.puletic@gmail.com>
2020-06-15 20:32:22 +0200

    updated sending message service

--- src/main/java/tim11osa/email/main_app/services/MessageService.java
+++ src/main/java/tim11osa/email/main_app/services/MessageService.java
@@ -4,6 +4,7 @@ import com.sun.istack.ByteArrayDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tim11osa.email.main_app.crud_interfaces.MessageInterface;
+import tim11osa.email.main_app.model.Attachment;
 import tim11osa.email.main_app.model.Message;
 import tim11osa.email.main_app.repository.MessageRepository;
 
@@ -41,6 +42,9 @@ public class MessageService implements MessageInterface {
     @Autowired
     MessageRepository messageRepository;
 
+    @Autowired
+    AccountRepository accountRepository;
+
     @Override
     public Set<Message> getAllMessages(int account_id) {
         return messageRepository.getAllmessagesForAccount(account_id);
@@ -49,63 +53,100 @@ public class MessageService implements MessageInterface {
     @Override
     public Message addNewMessage(Message message) {
 
-
         return messageRepository.save(message);
     }
 
-    @Autowired
-        AccountRepository accountRepository;
-
-        public boolean sendNewMessage(Message newMessage, int idAccount) {
-
-            boolean messageSent = true;
-
-            Account acc = accountRepository.findById(idAccount).get();
-
-            boolean isAuthenticationRequired = acc.isAuthentication();
 
 
-            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    public boolean sendNewMessage(Message newMessage, int idAccount) {
+
+        boolean messageSent = true;
+
+        Account acc = accountRepository.findById(idAccount).get();
+
+        boolean isAuthenticationRequired = acc.isAuthentication();
+
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
             //Properties props = mailSender.getJavaMailProperties();
-            Properties props = new Properties();
+        Properties props = new Properties();
             //SimpleMailMessage message = new SimpleMailMessage();
 
-            mailSender.setHost(acc.getSmtpAddress());
-            mailSender.setPort(acc.getSmtpPort()); //465 ...
+        mailSender.setHost(acc.getSmtpAddress());
+        mailSender.setPort(acc.getSmtpPort()); //465 ...
 
-            mailSender.setUsername(acc.getUsername());
-            mailSender.setPassword(acc.getPassword());
+        mailSender.setUsername(acc.getUsername());
+        mailSender.setPassword(acc.getPassword());
 
-            props.put("mail.transport.protocol", "smtp");
-            props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.debug", "true");
-            props.put("mail.smtp.ssl.trust", acc.getSmtpAddress());
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.ssl.trust", acc.getSmtpAddress());
 
-            if (isAuthenticationRequired) {
-                props.put("mail.smtp.auth", "true");
-            } else {
+        if (isAuthenticationRequired) {
+            props.put("mail.smtp.auth", "true");
+        } else {
                 //ovo bi trebalo za uns mejl
+        }
+
+        mailSender.setJavaMailProperties(props);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        boolean hasAttachments = false;
+        if (newMessage.getAttachments().size() > 0) hasAttachments = true;
+        MimeMessageHelper helper = null;
+
+        try {
+
+            helper = new MimeMessageHelper(mimeMessage, hasAttachments);
+
+            if (hasAttachments){
+
+                for (Attachment att : newMessage.getAttachments()){
+                    helper.addAttachment(att.getName(), new ByteArrayDataSource(att.getData(), createMimeType(att)));
+                }
+
             }
 
-            mailSender.setJavaMailProperties(props);
+            helper.setFrom(newMessage.getFrom());
+            helper.setTo(newMessage.getTo().stream().toArray(String[]::new));
+            helper.setCc(newMessage.getCc().stream().toArray(String[]::new));
+            helper.setBcc(newMessage.getBcc().stream().toArray(String[]::new));
 
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            helper.setSubject(newMessage.getSubject());
+            helper.setText(newMessage.getContent());
 
-            try {
-                boolean hasAttachments = false;
-                if (newMessage.getAttachments() == null  ) hasAttachments = false;
-                if ( newMessage.getAttachments() != null && newMessage.getAttachments().size() > 0) hasAttachments = true;
-                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, hasAttachments);
 
-                helper.setFrom(newMessage.getFrom());
-                helper.setTo(newMessage.getTo().stream().toArray(String[]::new));
-                helper.setCc(newMessage.getCc().stream().toArray(String[]::new));
-                helper.setBcc(newMessage.getBcc().stream().toArray(String[]::new));
+            mailSender.send(mimeMessage);
 
-                helper.setSubject(newMessage.getSubject());
-                helper.setText(newMessage.getContent());
+            newMessage.setActive(true); //setovao na frontu, mislim
+            newMessage.setAccount(acc); //moze na frontu
+            newMessage.setUnread(true); //ovo sam setovao na frontu
+            addNewMessage(newMessage);
+            // treba proci kroz sve cc bcc i to i naci njihove accounte i odraditi addNewMessage samo sa tim accom
 
-                String filename=newMessage.getAttachments().get(0).getName();
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            messageSent = false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            messageSent = false;
+        }
+
+
+        return messageSent;
+
+
+    }
+
+
+
+    private String createMimeType(Attachment att){
+            //att.getMime_type()
+
+            /*  String filename=newMessage.getAttachments().get(0).getName();
                 System.out.println(filename+"DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
 
                 String type= URLConnection.guessContentTypeFromName(filename+"."+newMessage.getAttachments().get(0).getMime_type());
@@ -113,37 +154,13 @@ public class MessageService implements MessageInterface {
                 System.out.println(type+"DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
                 DataSource dataSource = new ByteArrayDataSource(newMessage.getAttachments().get(0).getData(),type );
 
+*/
 
 
-                helper.addAttachment("asdsdaasd",dataSource);
-
-                //FileSystemResource file = new FileSystemResource("C:\\log.txt");
-
-                //helper.addAttachment(file.getFilename(), file);
-                //helper.addAttachment("aaa", new File("C://Users//Miljan//Desktop//download.png"));
-                mailSender.send(mimeMessage);
-
-                newMessage.setActive(true);
-                newMessage.setAccount(acc);
-                newMessage.setUnread(true);
-                addNewMessage(newMessage);
-                // treba proci kroz sve cc bcc i to i naci njihove accounte i odraditi addNewMessage samo sa tim accom
 
 
-            } catch (MessagingException e) {
-                e.printStackTrace();
-                messageSent = false;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                messageSent = false;
-            }
-
-
-            return messageSent;
-
-
-        }
+        return URLConnection.guessContentTypeFromName(att.getName()+"."+att.getMime_type());
+    }
 
 
 
